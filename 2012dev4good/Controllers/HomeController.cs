3585eff7956758321d261efecc8fef5c407a6b19
Swapneel <swapneel.d14@gmail.com>
2012-07-07 17:09:28 +0100

    next commit

--- 2012dev4good/Controllers/HomeController.cs
+++ 2012dev4good/Controllers/HomeController.cs
@@ -10,14 +10,41 @@ namespace _2012dev4good.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome...";
 
             return View();
         }
 
         public ActionResult About()
         {
+            System.Configuration.Configuration rootWebConfig =
+				System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/2012dev4good");
+			System.Configuration.ConnectionStringSettings connString = null;
+			if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0)
+			{
+				connString =
+					rootWebConfig.ConnectionStrings.ConnectionStrings["CMEntities"];
+				if (connString != null)
+					Console.WriteLine("CMEntities connection string = \"{0}\"",
+						connString.ConnectionString);
+				else
+					Console.WriteLine("No CMEntities connection string");
+			}
+
+            CreativeDetail cd = new CreativeDetail();
+            cd.UserId = "100";
+            cd.Title = "title";
+            cd.Body = "Body";
+            cd.Footer = "Footer";
+            CMEntities cm = new CMEntities(connString.ConnectionString);
+            cm.AddToCreativeDetails(cd);
+            cm.SaveChanges();
             return View();
         }
+
+        //public Action DisplayFeed()
+        //{
+
+        //}
     }
 }
